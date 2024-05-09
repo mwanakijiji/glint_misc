317c1e8b6c9c25248f38a38f5c60fc9384d57cf5
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-05-09 19:14:53 +1000

    correct hexabundle STL being produced!

--- notebooks/lenslet_hexabundle_array.py
+++ notebooks/lenslet_hexabundle_array.py
@@ -13,28 +13,11 @@ file_name = 'ips_nanoscribe_hexabundle_proj_20240508.STL'
 # Load the STL file
 your_mesh = mesh.Mesh.from_file(stem + file_name)
 
-# plot
-'''
-figure = plt.figure()
-axes = figure.add_subplot(projection='3d')
-
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-# Auto scale to the mesh size
-scale = your_mesh.points.flatten()
-axes.auto_scale_xyz(scale, scale, scale)
-
-axes.view_init(90, 90, 90)
-
-# Show the plot to the screen
-#plt.show()
-'''
-
 # Generate 61 different meshes so we can rotate them later
-meshes = [your_mesh for _ in range(61)]
+meshes = [mesh.Mesh(your_mesh.data.copy()) for _ in range(61)] # .data.copy() is important!
 
 # make hexagonal points where the lenslets will be
-points = Hexgrids.FiniteHexGrid(grid_spacing=0.114, rotation=0, origin=(0,0), hexagonal_radius=4)
+points = Hexgrids.FiniteHexGrid(grid_spacing=0.106, rotation=0, origin=(0,0), hexagonal_radius=4)
 coords = points.all_xy_points() # coords[:,0] are x; coords[:,1] are y
 
 # translation function
@@ -47,46 +30,29 @@ def translate(_solid, del_x, del_y, del_z=0):
 
     return solid_translated
 
+# combine multiple meshes into STL
 def combined_stl(meshes_pass, save_path="./combined.stl"):
     combined = mesh.Mesh(np.concatenate([m.data for m in meshes_pass]))
     combined.save(save_path, mode=stl.Mode.ASCII)
 
+
 # translate lenslets
 meshes2 = []
 for lenslet_num in range(0,len(meshes)):
-    
-    #import ipdb; ipdb.set_trace()
-    meshes2.append(translate(meshes[lenslet_num], del_x=coords[lenslet_num,0], del_y=coords[lenslet_num,1], del_z=0))
-    #meshes[lenslet_num] = translate(meshes[lenslet_num], del_x=coords[lenslet_num,0], del_y=coords[lenslet_num,1], del_z=0)
-
-    print('lenslet num',lenslet_num)
-    print('del_x\n',coords[lenslet_num,0])
-    print('original\n',meshes[lenslet_num].x)
-    print('translated (x):\n', translate(meshes[lenslet_num], del_x=coords[lenslet_num,0], del_y=coords[lenslet_num,1], del_z=0).x)
-    print('meshes2 (x):\n', meshes2[lenslet_num].x)
-    print('meshes2[lenslet_num].x[0][0]:\n', meshes2[lenslet_num].x[0][0])
-    print('--------')
+    meshes2.append(translate(meshes[lenslet_num], del_x=coords[lenslet_num,0], del_y=0, del_z=coords[lenslet_num,1]))
     
 
-import ipdb; ipdb.set_trace()
-
-meshes2 = [translate(meshes[lenslet_num], del_x=coords[lenslet_num,0], del_y=coords[lenslet_num,1], del_z=0) for lenslet_num in range(0,len(meshes))]
-
-
-meshes3 = []
-[meshes3.append(meshes[44]) for mesh in meshes]
-
-# plot hexagonal grid
+# FYI plot of hexagonal grid
+'''
 plt.scatter(coords[:,0],coords[:,1])
 plt.show()
+'''
 
-import ipdb; ipdb.set_trace()
+# save
+combined_stl(meshes_pass=meshes2, save_path="./junk_combined2.stl")
 
-combined_stl(meshes_pass=meshes, save_path="./combined.stl")
-combined_stl(meshes_pass=meshes2, save_path="./combined2.stl")
-
-import ipdb; ipdb.set_trace()
-
+'''
+# FYI plotting
 plt.clf()
 # plot in 3D
 figure = plt.figure()
@@ -104,3 +70,4 @@ axes.set_zlim([-5,5])
 axes.view_init(0, 90, 0)
 axes.set_box_aspect([1, 1, 1])
 plt.show()
+'''
